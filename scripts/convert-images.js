75bc4d6de9318f7d80ac4dd8a0680bb9e7a7b5d3
mac m3 <macm3@macs-MacBook-Pro.local>
2025-08-06 05:29:49 -0700

    resizw webp

--- scripts/convert-images.js
+++ scripts/convert-images.js
@@ -2,24 +2,68 @@ import sharp from "sharp";
 import fs from "fs";
 import path from "path";
 
-const srcDir = path.join(process.cwd(), "public/img");
-const outDir = path.join(process.cwd(), "public/img-optimized");
+// const srcDir = path.join(process.cwd(), "public/img");
+// const outDir = path.join(process.cwd(), "public/img-optimized");
+
+// if (!fs.existsSync(outDir)) fs.mkdirSync(outDir);
+
+// const files = fs.readdirSync(srcDir);
+
+// files.forEach(async (file) => {
+//   const ext = path.extname(file).toLowerCase();
+//   if (![".png", ".jpg", ".jpeg"].includes(ext)) return;
+
+//   const inputPath = path.join(srcDir, file);
+//   const outputPath = path.join(outDir, file.replace(ext, ".webp"));
+
+//   await sharp(inputPath)
+//     .resize({ width: 600 }) // автоуменьшение под реальный контейнер
+//     .webp({ quality: 70 }) // 60–75 — золотая середина
+//     .toFile(outputPath);
+
+//   console.log(`Optimized: ${file} → ${path.basename(outputPath)}`);
+// });
+
+const srcDir = path.join(process.cwd(), "public/img-optimized");
+const outDir = path.join(process.cwd(), "public/img-optimized-small");
 
 if (!fs.existsSync(outDir)) fs.mkdirSync(outDir);
 
-const files = fs.readdirSync(srcDir);
+const filesToResize = [
+  "avatar2.webp",
+  "Market3.webp",
+  "Managing3.webp",
+  "Market1.webp",
+  "Property1.webp",
+  "Property3.webp",
+  "Fundamentals1.webp",
+  "avatar1.webp",
+  "Financing1.webp",
+  "Exit3.webp",
+  "Managing1.webp",
+  "Exit1.webp",
+  "Financing3.webp",
+  "avatar3.webp",
+  "Managing2.webp",
+  "Market2.webp",
+  "Financing2.webp",
+  "Exit2.webp",
+  "Fundamentals2.webp",
+  "Property2.webp",
+];
 
-files.forEach(async (file) => {
-  const ext = path.extname(file).toLowerCase();
-  if (![".png", ".jpg", ".jpeg"].includes(ext)) return;
-
+// Логика: аватары → 48x48, остальные → 40x40
+for (const file of filesToResize) {
   const inputPath = path.join(srcDir, file);
-  const outputPath = path.join(outDir, file.replace(ext, ".webp"));
+  const outputPath = path.join(outDir, file);
 
-  await sharp(inputPath)
-    .resize({ width: 600 }) // автоуменьшение под реальный контейнер
-    .webp({ quality: 70 }) // 60–75 — золотая середина
-    .toFile(outputPath);
+  const size = file.startsWith("avatar") ? 48 : 40;
 
-  console.log(`Optimized: ${file} → ${path.basename(outputPath)}`);
-});
+  sharp(inputPath)
+    .resize({ width: size, height: size, fit: "cover" })
+    .webp({ quality: 70 })
+    .toFile(outputPath)
+    .then(() => console.log(`✔ ${file} → ${size}x${size}`))
+    .catch((err) => console.error(`Ошибка с ${file}:`, err));
+}
+
