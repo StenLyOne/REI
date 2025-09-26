372895465b21ba101f868300b8cf8f4192f87e46
Vlad <macm3@macs-MacBook-Pro.local>
2025-09-26 09:56:23 -0700

    fixed how reiWorks variants

--- app/sections/HowReiWorks/HowReiWorks.tsx
+++ app/sections/HowReiWorks/HowReiWorks.tsx
@@ -78,27 +78,7 @@ const data = [
 ];
 
 export default function HowReiWorks() {
-  const pathVariants = {
-    hidden: { pathLength: 0, pathOffset: 1, opacity: 0 },
-    visible: {
-      pathLength: 1,
-      pathOffset: 0,
-      opacity: 1,
-      transition: {
-        duration: 2,
-        ease: "easeOut",
-      },
-    },
-  };
 
-  const itemVariants = {
-    hidden: { opacity: 0, y: 30 },
-    visible: {
-      opacity: 1,
-      y: 0,
-      transition: { ease: "easeOut" },
-    },
-  };
 
   const containerVariants = {
     hidden: { opacity: 0 },
@@ -123,7 +103,7 @@ export default function HowReiWorks() {
       >
         <div>
           <div className="mx-auto space-y-[24px] text-center max-w-[820px]">
-            <motion.h2 className="h2-large" variants={itemVariants}>
+            <motion.h2 className="h2-large">
               Why the Industry Is
               <span className="gradient-text text-bold"> Shifting</span>
             </motion.h2>
@@ -154,11 +134,11 @@ export default function HowReiWorks() {
           </div>
         </div>
 
-        <motion.div className="mt-15" >
+        <motion.div className="mt-15">
           <ReiLabel />
         </motion.div>
 
-        <motion.div className="relative w-full h-full" >
+        <motion.div className="relative w-full h-full">
           <div className="absolute top-0 bottom-0 left-1/2 transform -translate-x-1/2 max-w-[400px] w-full h-[600px]">
             {/* MOBILE lineS */}
             <div className="absolute left-1/2 -translate-x-1/2 top-[-35px] w-full max-w-[380px] h-[716px] max-[600px]:px-[60px] justify-between pointer-events-none z-0 hidden max-[1024px]:flex">
@@ -173,42 +153,34 @@ export default function HowReiWorks() {
                 transition={{ duration: 2, ease: "easeOut" }}
               >
                 <motion.path
-                  variants={pathVariants}
                   d="M0 109.5C0 109.5 82 109.5 85.5 109.5C89 109.5 113 109.5 121.5 101C130 92.5 129.5 67.5 129.5 67.5V0"
                   stroke="#B34AE4"
                 />
                 <motion.path
-                  variants={pathVariants}
                   d="M0 312.5C0 312.5 82 312.5 85.5 312.5C89 312.5 113 312.5 121.5 304C130 295.5 129.5 270.5 129.5 270.5V0"
                   stroke="#B34AE4"
                 />
                 <motion.path
-                  variants={pathVariants}
                   d="M0 513.5C0 513.5 82 513.5 85.5 513.5C89 513.5 113 513.5 121.5 505C130 496.5 129.5 471.5 129.5 471.5V0"
                   stroke="#B34AE4"
                 />
                 <motion.path
-                  variants={pathVariants}
                   d="M258.505 109.5C258.505 109.5 176.505 109.5 173.005 109.5C169.505 109.5 145.505 109.5 137.005 101C128.505 92.5 129.005 67.5 129.005 67.5V0"
                   stroke="#B34AE4"
                 />
                 <motion.path
-                  variants={pathVariants}
                   d="M258.505 312.5C258.505 312.5 176.505 312.5 173.005 312.5C169.505 312.5 145.505 312.5 137.005 304C128.505 295.5 129.005 270.5 129.005 270.5V0"
                   stroke="#B34AE4"
                 />
                 <motion.path
-                  variants={pathVariants}
                   d="M258.505 513.5C258.505 513.5 176.505 513.5 173.005 513.5C169.505 513.5 145.505 513.5 137.005 505C128.505 496.5 129.005 471.5 129.005 471.5V0"
                   stroke="#B34AE4"
                 />
                 <motion.path
-                  variants={pathVariants}
                   d="M0 715.5C0 715.5 82 715.5 85.5 715.5C89 715.5 113 715.5 121.5 707C130 698.5 129.5 673.5 129.5 673.5V202"
                   stroke="#B34AE4"
                 />
                 <motion.path
-                  variants={pathVariants}
                   d="M258.505 715.5C258.505 715.5 176.505 715.5 173.005 715.5C169.505 715.5 145.505 715.5 137.005 707C128.505 698.5 129.005 673.5 129.005 673.5V0.129836"
                   stroke="#B34AE4"
                 />
@@ -228,42 +200,34 @@ export default function HowReiWorks() {
                 transition={{ duration: 2, ease: "easeOut" }}
               >
                 <motion.path
-                  variants={pathVariants}
                   d="M1 143.75C1 -0.253509 561 108.627 561 0.75"
                   stroke="#B34AE4"
                 />
                 <motion.path
-                  variants={pathVariants}
                   d="M1121 143.75C1121 -0.253509 561 108.627 561 0.75"
                   stroke="#B34AE4"
                 />
                 <motion.path
-                  variants={pathVariants}
                   d="M137 205.75C137 62.25 561 108.25 561 0.75"
                   stroke="#B34AE4"
                 />
                 <motion.path
-                  variants={pathVariants}
                   d="M985 205.75C985 62.25 561 108.25 561 0.75"
                   stroke="#B34AE4"
                 />
                 <motion.path
-                  variants={pathVariants}
                   d="M294 288.75C294 145 561 108.437 561 0.75"
                   stroke="#B34AE4"
                 />
                 <motion.path
-                  variants={pathVariants}
                   d="M828 288.75C828 145 561 108.437 561 0.75"
                   stroke="#B34AE4"
                 />
                 <motion.path
-                  variants={pathVariants}
                   d="M470 291.75C470 148.003 561 108.435 561 0.75"
                   stroke="#B34AE4"
                 />
                 <motion.path
-                  variants={pathVariants}
                   d="M652 291.75C652 147.977 561 108.454 561 0.75"
                   stroke="#B34AE4"
                 />
